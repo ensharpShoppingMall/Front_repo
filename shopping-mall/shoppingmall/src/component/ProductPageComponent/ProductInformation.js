f209cc2d6fbf7432c19b6234b310df4e9cba51a2
“sangjun121” <juncho12011201@gmail.com>
2023-06-26 01:32:50 +0900

    [ADD] 상품 상세페이지 좌측 설명란 옵션 다른 브랜치에서 가져와서 적용 및 모든 옵션 추가 완료

--- shopping-mall/shoppingmall/src/component/ProductPageComponent/ProductInformation.js
+++ shopping-mall/shoppingmall/src/component/ProductPageComponent/ProductInformation.js
@@ -1,8 +1,12 @@
+import '@popperjs/core';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/ProductPage.css';
 
 const ProductInformation = () => {
     return (
-        <div>
+        <div id="product-information">
             <div className="information-container">
                 <div className="product-information">
                     <div className="product-detail">
@@ -56,10 +60,87 @@ const ProductInformation = () => {
                     <div className="naver-pay">
                         <button className="naver-pay-button">NAVER PAY</button>
                     </div>
-                    <div className="size-guide"></div>
+                    <div
+                        class="accordion accordion-flush"
+                        id="accordionFlushExample"
+                    >
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button
+                                    class="accordion-button collapsed"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseOne"
+                                    aria-expanded="false"
+                                    aria-controls="flush-collapseOne"
+                                >
+                                    SIZE GUIDE
+                                </button>
+                            </h2>
+                            <div
+                                id="flush-collapseOne"
+                                class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample"
+                            >
+                                <div class="accordion-body">사이즈 기입</div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button
+                                    class="accordion-button collapsed"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTwo"
+                                    aria-expanded="false"
+                                    aria-controls="flush-collapseTwo"
+                                >
+                                    FABRIC & CARE
+                                </button>
+                            </h2>
+                            <div
+                                id="flush-collapseTwo"
+                                class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample"
+                            >
+                                <div class="accordion-body">
+                                    뭐쓰는지 모르겠지만 일단 기입
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button
+                                    class="accordion-button collapsed"
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThree"
+                                    aria-expanded="false"
+                                    aria-controls="flush-collapseThree"
+                                >
+                                    SHIPPING & RETURNS
+                                </button>
+                            </h2>
+                            <div
+                                id="flush-collapseThree"
+                                class="accordion-collapse collapse"
+                                data-bs-parent="#accordionFlushExample"
+                            >
+                                <div class="accordion-body">
+                                    " SHIPPING"
+                                    <br />- 아모멘토는 CJ대한통운과 배송대행
+                                    계약이 체결되어 있습니다.
+                                    <br />- 배송비
+                                    <br />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <div className="size-guide"></div>
                     <div className="fabric-care"></div>
                     <div className="shipping-return"></div>
-                    <div className="customer-service"></div>
+    <div className="customer-service"></div>*/}
                 </div>
                 <div className="product-image">상준</div>
                 <div className="empty-space">상준</div>
